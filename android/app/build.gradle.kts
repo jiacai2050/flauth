c09701eecc300edd42601dde244c29396285654b
jiacai2050 <dev@liujiacai.net>
2026-01-01 17:22:21 +0800

    update flowchart

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,8 +26,9 @@ android {
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "net.liujiacai.flauth"
-        // Local auth requires minSdkVersion 28. See:
+        // Local auth requires minSdkVersion 28(Android 9 Pie, released at August 6, 2018). See:
         // https://github.com/flutter/packages/tree/local_auth-v3.0.0/packages/local_auth/local_auth#android-theme
+        // https://en.wikipedia.org/wiki/Android_version_history
         minSdk = 28
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
